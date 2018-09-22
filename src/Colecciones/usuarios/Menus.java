9a68a4851ddadd0b6f3a9dcaeb56e4870e03842b
user2 <info@developandsys.com>
2018-09-22 09:00:38 +0200

    Ejercicios ficheros

---
+++ src/Colecciones/usuarios/Menus.java
@@ -0,0 +1,18 @@
+package Colecciones.usuarios;
+
+public class Menus {
+
+    protected String menuGenerico() {
+
+        String menu = "*** Gestión de alumnos *** \n" +
+                "1.- Crear alumnos \n" +
+                "2.- Buscar alumnos \n" +
+                "3.- Borrar alumnos\n" +
+                "4.- Listar alumnos 3D\n" +
+                "5.- Exportar alumnos\n" +
+                "6.- Cargar alumnos\n"+
+                "7.- Salir\n" +
+                "Introduce una opción: ";
+        return menu;
+    }
+}
